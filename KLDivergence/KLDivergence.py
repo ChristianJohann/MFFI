d4d17a5de66f0e4ae289ff274235e88eb8724fdd
Christian Johann Liegl <Christian.Liegl@gmail.com>
2021-01-30 22:11:51 +0100

    Update KLDivergence.py

--- KLDivergence/KLDivergence.py
+++ KLDivergence/KLDivergence.py
@@ -8,7 +8,7 @@ def kl_divergence(p, q, base=None):
     p: a numpy array specifying the reference pdf or pmf
     q: a numpy array specifying an alternative pdf or pmf
     base: optional argument specifying the base of the logarithm
-    """"
+    """
     output = np.sum(np.where(p != 0, p * np.log(p / q), 0)) # excludes infinite values from calculation
     if base is not None:
         output /= np.log(base)
