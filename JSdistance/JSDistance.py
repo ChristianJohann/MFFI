a7e7752d8c8c3930c91ea543cf6575b3c984ce0b
Christian Johann Liegl <Christian.Liegl@gmail.com>
2021-01-30 22:13:13 +0100

    Update JSDistance.py

--- JSdistance/JSDistance.py
+++ JSdistance/JSDistance.py
@@ -8,7 +8,7 @@ def JSdistance(p, q, base = None):
     p: a list of floats or numpy array specifying a pdf or pmf
     q: a list of floats or numpy array specifying a pdf or pmf
     base: optional argument specifying the base of the logarithm
-    """"
+    """
     if base is not None:
         output = distance.jensenshannon(p, q, base)
     else:
