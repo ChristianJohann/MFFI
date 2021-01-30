5a602b43e645f8d3affeb16933a5cad7dd948611
Christian Johann Liegl <Christian.Liegl@gmail.com>
2021-01-30 22:07:48 +0100

    Update JSDistance.py

--- JSdistance/JSDistance.py
+++ JSdistance/JSDistance.py
@@ -1,6 +1,14 @@
 from scipy.spatial import distance
 
 def JSdistance(p, q, base = None):
+    """
+    calculates the Jenson-Shannon distance between two distributions
+    
+    Input:
+    p: a list of floats or numpy array specifying a pdf or pmf
+    q: a list of floats or numpy array specifying a pdf or pmf
+    base: optional argument specifying the base of the logarithm
+    """"
     if base is not None:
         output = distance.jensenshannon(p, q, base)
     else:
