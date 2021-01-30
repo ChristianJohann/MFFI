d0d052fbde612added2cca34c82e9a52698c1d3f
Christian Johann Liegl <Christian.Liegl@gmail.com>
2021-01-30 22:10:33 +0100

    Update KLDivergence.py

--- KLDivergence/KLDivergence.py
+++ KLDivergence/KLDivergence.py
@@ -1,8 +1,15 @@
 import numpy as np
 
 def kl_divergence(p, q, base=None):
-    output = np.sum(np.where(p != 0, p * np.log(p / q), 0))
+    """
+    calculates the Kullback-Leibler divergence between two distributions
+    
+    Input:
+    p: a numpy array specifying the reference pdf or pmf
+    q: a numpy array specifying an alternative pdf or pmf
+    base: optional argument specifying the base of the logarithm
+    """"
+    output = np.sum(np.where(p != 0, p * np.log(p / q), 0)) # excludes infinite values from calculation
     if base is not None:
         output /= np.log(base)
     return output
-
